f166e7d2da880b2410df42501a678910f2a1e6ca
Ardiannn08 <ardian@tokopedia.com>
2022-11-30 15:59:54 +0700

    Resolve Outdated Comment v1

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# resolve-outdated-comment
+# Resolve Outdated Comment
+
+To keep pull requests free of flooded comments, automatically resolve or remove `outdated` comments on pull requests from a specified GitHub user bot.
+
+This feature will be useful when we have bots to review. Maybe there are a lot of comments from bot review, and after the requestor has fixed the comments, everything that is 'out of date' is still there, and it will make comments from other reviewers pile up or even make other reviewers not aware which parts are completely resolved and which are not.
+
+
+**Notes:**
+```
+This flow is far from perfect, 
+but it aims to produce the most frictionless experience possible considering technological limitations.
+```
+
+# Parameter
+- **token**: GitHub Personal Access Token.
+- **filter-user**: specify the checked bot user. 
+- **mode**: `delete` / `resolve`
+  - *delete*: remove an `oudated` comment completely.
+  - *resolve*: resolve the conversation of `outdated` comment.
+
+# Example Workflow
+
+```yaml
+name: "Auto Resolve Comment"
+on: push
+jobs:
+  required_label:
+    name: "Auto Resolve Comment"
+    runs-on: ubuntu-latest
+    steps:
+      - uses: Ardiannn08/resolve-outdated-comment@v1.1
+        with:
+          token: ${{ secrets.GH_TOKEN }}
+          filter-user: "your-bot-name"
+          mode: "delete"
+```
+
+# Contribution
+For now I don't think there will be any contributors, if you have problems or ideas for improvement, feel free to open an issue. I would ❤️ contributions to improve this action. 
---
+++ src/index.js
@@ -0,0 +1,163 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const accessToken = core.getInput('token');
+const octokit = github.getOctokit(accessToken);
+const filterUser = core.getInput('filter-user');;
+const mode = core.getInput('mode') || 'resolve';
+
+async function run() {
+  const edges = await getAllReviewThreadList();
+  const filteredEdges = edges.filter((edge) => {
+    const noOtherAuthorComments = edge.node.comments.edges.length == 1;
+    const oudatedAndNeedToResolve = edge.node.isOutdated && !edge.node.isResolved
+    return edge.node.comments.edges[0].node.author.login == filterUser && noOtherAuthorComments && oudatedAndNeedToResolve;
+  });
+
+  for (const edge of filteredEdges) {
+    switch (mode) {
+      case 'resolve':
+        resolveThread(edge.node.id);
+        break;
+      case 'delete':
+        deletePullRequestCommentReview(edge.node.comments.edges[0].node.id);
+        break;
+      default:
+        core.setFailed(`unknown mode ${mode}`)
+        break;
+    }
+  }
+}
+
+async function deletePullRequestCommentReview(id) {
+  const mutation = `
+    mutation DeletePullRequestReviewComment($id:ID!){
+      deletePullRequestReviewComment(input:{
+        id:$id
+      }) {
+        clientMutationId
+      }
+    }
+  `
+
+  const parameter = {
+    id: id
+  }
+
+  await octokit.graphql(mutation, parameter)
+}
+
+async function resolveThread(id) {
+  const mutation = `
+    mutation ResolveThread($id:ID!){
+      resolveReviewThread(input:{
+        threadId:$id
+      }) {
+        clientMutationId
+      }
+    }
+  `;
+
+  const parameter = {
+    id: id
+  };
+
+  await octokit.graphql(mutation, parameter);
+}
+
+async function getAllReviewThreadList() {
+  const { edges, page_info } = await getReviewThreadList(null);
+  if (page_info.hasNextPage) {
+    const res = await getReviewThreadList(page_info.endCursor);
+    return [...edges, ...res.edges];
+  }
+
+  return edges;
+}
+
+async function getReviewThreadList(cursor) {
+  const query = `
+    query GetReviewThreadList($repo_owner:String!, $repo_name:String!, $pull_request_number:Int!, $next_cursor:String){
+      repository(owner:$repo_owner, name:$repo_name) {
+        id
+        pullRequest(number:$pull_request_number) {
+          id
+          reviewThreads(first:100, after: $next_cursor) {
+            pageInfo{
+              hasNextPage
+              endCursor
+            }
+            edges {
+              node {
+                id
+                comments(first:2){
+                  edges{
+                    node{
+                      id
+                      body
+                      author{
+                        login
+                      }
+                    }
+                  }
+                }
+                isOutdated
+                isResolved
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const parameter = {
+    repo_owner: github.context.repo.owner,
+    repo_name: github.context.repo.repo,
+    pull_request_number: await getPullRequestNumber(),
+    next_cursor: cursor
+  }
+
+  const result = await octokit.graphql(query, parameter);
+  return {
+    edges: result.repository.pullRequest.reviewThreads.edges,
+    page_info: result.repository.pullRequest.reviewThreads.pageInfo
+  }
+}
+
+async function getPullRequestNumber() {
+  if (github.context.payload.number) {
+    return github.context.payload.number;
+  }
+
+  const branchName = github.context.payload.pull_request.head.ref;
+  if (branchName == undefined) {
+    core.setFailed(`branch ${branchName} is invalid`);
+  }
+
+  const query = `
+    query GetPullRequest($repo_owner:String!, $repo_name:String!, $head_ref:String!) { 
+      repository(owner:$repo_owner, name:$repo_name) {
+        id
+        pullRequests(headRefName:$head_ref, first: 1, states: OPEN) {
+          edges {
+            node {
+              id
+              number
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const parameter = {
+    repo_owner: github.context.repo.owner,
+    repo_name: github.context.repo.repo,
+    head_ref: branchName
+  }
+
+  const result = await octokit.graphql(query, parameter);
+  return result.repository.pullRequests.edges[0].node.number;
+}
+
+run();
